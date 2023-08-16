149bef7a33386a51c31a405a4a87665d7e280b6e
vankoreaseoul <gofltmxj@naver.com>
2023-08-16 13:58:20 +0900

    Initial Commit

---
+++ CameraTest/CameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestApp.swift
+//  CameraTest
+//
+//  Created by Heawon Seo on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct CameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
